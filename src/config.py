60e09d9374a8a80830dd1e956641b71b4c25494a
uddithmachiraju <uddithmachiraju@gmail.com>
2025-04-26 20:05:17 +0530

    Updates the Configuration

--- src/config.py
+++ src/config.py
@@ -2,9 +2,15 @@ from pathlib import Path
 
 raw_data_path = Path("data/raw/Reviews.csv")
 preprocessed_data_path = Path("data/processed/processed.csv")
+train_path = Path("data/processed/train.csv")
+eval_path = Path("data/processed/eval.csv")
+test_path = Path("data/processed/test.csv") 
 model_saving_path = Path("models/model.pkl")
 
 # Ensure folders exist, not files
 raw_data_path.parent.mkdir(parents = True, exist_ok = True)
 preprocessed_data_path.parent.mkdir(parents = True, exist_ok = True)
+train_path.parent.mkdir(parents = True, exist_ok = True)
+eval_path.parent.mkdir(parents = True, exist_ok = True)
+test_path.parent.mkdir(parents = True, exist_ok = True)
 model_saving_path.parent.mkdir(parents = True, exist_ok = True)
