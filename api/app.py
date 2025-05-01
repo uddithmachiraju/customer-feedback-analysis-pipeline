8462e7be5736f4e1c6b384015ceb9df1b0d48f20
uddithmachiraju <uddithmachiraju@gmail.com>
2025-05-01 09:00:40 +0530

    Fixed the Endpoint error for docker

--- api/app.py
+++ api/app.py
@@ -37,7 +37,7 @@ def home():
     return render_template("index.html", prediction=None)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 # Text	Expected Sentiment
 # "The service was terrible and the product broke within a week."	Negative
